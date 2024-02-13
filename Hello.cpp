83a4b9b556055dc527a398b8e2ed62dfbc7e01ee
JustinFowler8285 <008285349@coyote.csusb.edu>
2024-02-13 14:56:25 -0800

    Initial commit
    Addedd

---
+++ Hello.cpp
@@ -0,0 +1,14 @@
+/***
+ * {Short Description of the lab}
+***/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    printf("Hello World again!\n");
+    return 0;
+
+}
